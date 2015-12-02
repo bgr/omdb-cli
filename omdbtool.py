46cbcfe457f12db35595d4938e36eaf7511f0b6b
Korons <william.sweeting@vfemail.net>
2015-12-02 04:47:15 -0500

    Renamed to omdbtool.py. Added new search ability
    Added the --type option allowing for search of movies/series/episodes 
    Added the --season option allowing for search by season number
    Added the --episode option allowing for search by episode

--- omdbtool.py
+++ omdbtool.py
@@ -5,7 +5,7 @@ import urllib
 import sys
 import json
 
-parser = argparse.ArgumentParser(description='Get IMDb data for a movie')
+parser = argparse.ArgumentParser(description='Get OMDb data for a movie')
 
 parser.add_argument("-t", help="Movie title")
 parser.add_argument("-y", help="Year of release", type=int)
@@ -13,11 +13,14 @@ parser.add_argument("-i", help="IMDb movie id")
 parser.add_argument("-r", help="Return raw XML/JSON response", choices=['JSON','XML'])
 parser.add_argument("--plot", help="Length of plot summary", choices=['short','full'])
 parser.add_argument("--tomatoes", help="Include Rotten Tomatoes data too", action="store_true")
+parser.add_argument("--type", help="movie, series, episode", choices=['movie','series','episode'])
+parser.add_argument("--season", help="Season number", type=int)
+parser.add_argument("--episode", help="episode number", type=int)
 
 args = parser.parse_args()
 
 params = {}
-keys = ['t', 'y', 'i', 'plot', 'r', 'tomatoes']
+keys = ['t', 'y', 'i', 'plot', 'r', 'tomatoes','type','season','episode']
 
 for k in keys:
   if args.__getattribute__(k): params[k] = args.__getattribute__(k)
@@ -27,7 +30,7 @@ if len(params) == 0:
   sys.exit()
 
 
-### call IMDb API
+### call OMDb API
 
 apicall = urllib.urlopen('http://www.omdbapi.com/?%s' % urllib.urlencode(params))
 result = apicall.read()
